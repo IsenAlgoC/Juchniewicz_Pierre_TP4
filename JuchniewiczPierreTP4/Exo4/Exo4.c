c263e4bef2b8f4f2451eca4436b7098e8dd1513e
Pierre JUCHNIEWICZ <pierre.juchniewicz@hei.yncrea.fr>
2020-11-14 15:18:07 +0100

    Fin Exo4

--- JuchniewiczPierreTP4/Exo4/Exo4.c
+++ JuchniewiczPierreTP4/Exo4/Exo4.c
@@ -5,12 +5,13 @@
 #define TAILLEMAXMOT 20
 
 int main() {
-	char mot[TAILLEMAXMOT];
+	char mot[TAILLEMAXMOT];							//initialisation des variables
 	int palindrome = 0;
 	printf("inserez votre mot : ");
 	scanf_s("%s", mot, (unsigned)_countof(mot));	//on insère notre mot
+	printf("Votre mot '%s' ", mot);					//indique le mot inséré 
 
-	for (int i = 0; i < TAILLEMAXMOT; ++i) {								//on enlève toute majuscule, tout accent...
+	for (int i = 0; i < TAILLEMAXMOT; ++i) {		//on enlève toute majuscule, tout accent...
 		mot[i] = tolower(mot[i]);
 		if ((mot[i] == 'é') || (mot[i] == 'è') || (mot[i] == 'ê')){
 			mot[i] = 'e';
@@ -23,14 +24,17 @@ int main() {
 		}
 	}
 
-	if (strlen(mot) % 2 == 0) {
-		for (int i = 0; i < strlen(mot)/2; ++i) {
-			if (mot[i] == mot[strlen(mot) - i]) {
-				palindrome = palindrome + 1;
-			}
+	for (int i = 0; i < strlen(mot); ++i) {			// on parcours i sur toute la longueur du mot
+		if (mot[i] == mot[(strlen(mot) - 1) - i]) {	// si i == la lettre symétriquement opposé, alors palindrome +=1
+			palindrome += 1;		
 		}
+
 	}
 
-
-	
+	if (palindrome == strlen(mot)) {				//si toutes les lettres respectent la symétrie du mot, alors c'est un palindrome
+		printf("est un palindrome");
+	}
+	else {
+		printf("n'est pas un palindrome");			//sinon s'en est pas un 
+	}
 }
